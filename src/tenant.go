6ab628b2fcfb5974ae000b9bc790b743bb64bc37
Mike Taylor <mike@indexdata.com>
2025-04-22 11:05:45 +0100

    [MODREP-25] Provide some report repositories as sample data (#114)
    No change-logging yet, and test coverage to be added. For now, I need to see whether this works when snapshot gets rebuilt, and for that it has to be merged to master.

---
+++ src/tenant.go
@@ -0,0 +1,75 @@
+package main
+
+import "io"
+import "fmt"
+import "net/http"
+import "encoding/json"
+import "github.com/indexdata/foliogo"
+
+type jsonTenantParameters struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type jsonTenantApi struct {
+	Module_to  string                 `json:"module_to"`
+	Parameters []jsonTenantParameters `json:"parameters"`
+}
+
+func handleTenantAPI(w http.ResponseWriter, req *http.Request, session *ModReportingSession) error {
+	bytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return fmt.Errorf("could not read HTTP request body: %w", err)
+	}
+
+	var tenantApi jsonTenantApi
+	err = json.Unmarshal(bytes, &tenantApi)
+	if err != nil {
+		return fmt.Errorf("could not deserialize JSON from body: %w", err)
+	}
+	session.Log("tenant", "module_to:", tenantApi.Module_to, "-- parameters:", fmt.Sprintf("%+v", tenantApi.Parameters))
+	for _, param := range tenantApi.Parameters {
+		if param.Key == "loadSample" && param.Value == "true" {
+			return loadSample(w, req, session)
+		}
+	}
+
+	return nil
+}
+
+func loadSample(w http.ResponseWriter, req *http.Request, session *ModReportingSession) error {
+	id := "bd76ccec-2942-41f2-9bde-38f562d41842"
+	body := fmt.Sprintf(`{
+  "id": "%s",
+  "scope": "ui-ldp.admin",
+  "key": "tqrepos",
+  "value": [
+    {
+      "type": "gitlab",
+      "user": "MikeTaylor",
+      "repo": "metadb-queries",
+      "branch": "main",
+      "dir": "folio/reports"
+    },
+    {
+      "user": "metadb-project",
+      "repo": "metadb-examples",
+      "branch": "main",
+      "dir": "folio/reports"
+    }
+  ]
+}`, id)
+	session.Log("tenant", "loading sample data")
+	_, err := fetchWithToken(req, session.folioSession, "settings/entries", foliogo.RequestParams{
+		Method:      "POST",
+		Body:        body,
+		ContentType: "application/json",
+	})
+	if err != nil {
+		return fmt.Errorf("could not POST to mod-settings: %w", err)
+	}
+
+	// XXX Should we allow for 304 if the ID already exists?
+
+	return nil
+}
